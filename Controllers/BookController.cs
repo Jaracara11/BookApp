d6770df47e9e02e598e63d9515da9812c56c7035
Luis_Eduardo <undernet1@gmail.com>
2023-05-13 10:19:57 -0400

    Add project files.

---
+++ Controllers/BookController.cs
@@ -0,0 +1,45 @@
+﻿using BookApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.Text.Json;
+
+namespace BookApp.Controllers
+{
+    [ApiController]
+    [Route("api/book")]
+    public class BookController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<Book>> GetBookInfo()
+        {
+            string filePath = Path.Combine("data", "ISBN_Input_File.txt");
+            string isbn = System.IO.File.ReadAllText(filePath);
+
+            string apiUrl = $"https://openlibrary.org/isbn/{isbn}";
+            using (var client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+                    var stream = new MemoryStream(Encoding.UTF8.GetBytes(json));
+                    var data = await JsonSerializer.DeserializeAsync<Book>(stream);
+                    var book = new Book
+                    {
+                        ISBN = data.ISBN,
+                        Title = data.Title,
+                        Subtitle = data.Subtitle,
+                        AuthorName = data.AuthorName,
+                        NumberOfPages = data.NumberOfPages,
+                        PublishDate = data.PublishDate
+                    };
+                    return Ok(book);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+        }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace BookApp.Models
+{
+    public class Book
+    {
+        public string ISBN { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+        public string Subtitle { get; set; } = string.Empty;
+        public string AuthorName { get; set; } = string.Empty;
+        public int NumberOfPages { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+}
